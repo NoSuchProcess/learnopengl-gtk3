dc2b7af9764e358fc011678ac611d507cb20e64e
Gitea <no.such.process@gmail.com>
2019-08-23 02:37:09 +0500

    add lesson 5.8.3

---
+++ 5.8.3/shader_make.c
@@ -0,0 +1,47 @@
+#include <glib.h>
+#include <shader_compile.h>
+#include <shader_make.h>
+
+GLuint shader_make(int index)
+{
+	GLuint vertex, fragment;
+	GLuint program;
+	GLint success;
+
+	extern const void _binary____shader_vertex_glsl_start;
+	extern const void _binary____shader_vertex_glsl_end;
+	extern const void _binary____shader_fragment0_glsl_start;
+	extern const void _binary____shader_fragment0_glsl_end;
+	extern const void _binary____shader_fragment1_glsl_start;
+	extern const void _binary____shader_fragment1_glsl_end;
+
+	vertex = shader_compile(GL_VERTEX_SHADER, &_binary____shader_vertex_glsl_start, &_binary____shader_vertex_glsl_end - &_binary____shader_vertex_glsl_start);
+	switch (index) {
+	case 0:
+		fragment = shader_compile(GL_FRAGMENT_SHADER, &_binary____shader_fragment0_glsl_start, &_binary____shader_fragment0_glsl_end - &_binary____shader_fragment0_glsl_start);
+		break;
+	case 1:
+		fragment = shader_compile(GL_FRAGMENT_SHADER, &_binary____shader_fragment1_glsl_start, &_binary____shader_fragment1_glsl_end - &_binary____shader_fragment1_glsl_start);
+		break;
+	default:
+		fragment = 0;
+		break;
+	}
+
+	program = glCreateProgram();
+	glAttachShader(program, vertex);
+	glAttachShader(program, fragment);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE) {
+		GLchar message[512];
+
+		glGetProgramInfoLog(program, sizeof message, NULL, message);
+		g_error("Link error: %s\n", message);
+		program = 0;
+	}
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+
+	return program;
+}
