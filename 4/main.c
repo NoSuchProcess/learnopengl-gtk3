96563a302b153f4b8b54fb50005706afbd817290
Gitea <no.such.process@gmail.com>
2019-08-23 02:37:09 +0500

    add lesson 4

---
+++ 4/main.c
@@ -0,0 +1,23 @@
+#include <gtk/gtk.h>
+
+static void activate(GtkApplication *application, gpointer user_data)
+{
+	GtkWidget *window;
+
+	window = gtk_application_window_new(application);
+
+	gtk_widget_show_all(window);
+}
+
+int main(int argc, char *argv[])
+{
+	int result;
+	GtkApplication *application;
+
+	application = gtk_application_new(NULL, G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(G_OBJECT(application), "activate", G_CALLBACK(activate), NULL);
+	result = g_application_run(G_APPLICATION(application), argc, argv);
+	g_object_unref(G_OBJECT(application));
+
+	return result;
+}
