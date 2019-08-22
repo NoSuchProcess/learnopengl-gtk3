3bd484c40d9f4964173497d5ac7ab523a43c4014
Gitea <no.such.process@gmail.com>
2019-08-23 02:37:09 +0500

    add lesson 4.6

---
+++ 4.6/main.c
@@ -0,0 +1,38 @@
+#include <gtk/gtk.h>
+#include <epoxy/gl.h>
+
+static gboolean render(GtkGLArea *area, GdkGLContext *context, gpointer user_data)
+{
+	glClearColor(0.2, 0.3, 0.3, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	return TRUE;
+}
+
+static void activate(GtkApplication *application, gpointer user_data)
+{
+	GtkWidget *window;
+	GtkWidget *drawing;
+
+	drawing = gtk_gl_area_new();
+	g_signal_connect(G_OBJECT(drawing), "render", G_CALLBACK(render), NULL);
+
+	window = gtk_application_window_new(application);
+	gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+	gtk_container_add(GTK_CONTAINER(window), drawing);
+
+	gtk_widget_show_all(window);
+}
+
+int main(int argc, char *argv[])
+{
+	int result;
+	GtkApplication *application;
+
+	application = gtk_application_new(NULL, G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(G_OBJECT(application), "activate", G_CALLBACK(activate), NULL);
+	result = g_application_run(G_APPLICATION(application), argc, argv);
+	g_object_unref(G_OBJECT(application));
+
+	return result;
+}
