02b0501a72bb5c6bf48d532c780bb8ccc7a98d3f
Gitea <no.such.process@gmail.com>
2019-08-25 02:00:50 +0500

    add lesson 7.6

---
+++ 7.6/shader_make.c
@@ -0,0 +1,35 @@
+#include <glib.h>
+#include <shader_compile.h>
+#include <shader_make.h>
+
+GLuint shader_make()
+{
+	GLuint vertex, fragment;
+	GLuint program;
+	GLint success;
+
+	extern const void _binary____shader_vertex_glsl_start;
+	extern const void _binary____shader_vertex_glsl_end;
+	extern const void _binary____shader_fragment_glsl_start;
+	extern const void _binary____shader_fragment_glsl_end;
+
+	vertex = shader_compile(GL_VERTEX_SHADER, &_binary____shader_vertex_glsl_start, &_binary____shader_vertex_glsl_end - &_binary____shader_vertex_glsl_start);
+	fragment = shader_compile(GL_FRAGMENT_SHADER, &_binary____shader_fragment_glsl_start, &_binary____shader_fragment_glsl_end - &_binary____shader_fragment_glsl_start);
+
+	program = glCreateProgram();
+	glAttachShader(program, vertex);
+	glAttachShader(program, fragment);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE) {
+		GLchar message[512];
+
+		glGetProgramInfoLog(program, sizeof message, NULL, message);
+		g_error("Link error: %s\n", message);
+		program = 0;
+	}
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+
+	return program;
+}
