087e803387d182144a1e497c198794ee56dd2a9b
Sergey Stepanov <no.such.process@gmail.com>
2019-09-08 15:27:03 +0500

    add lesson 16.1

---
+++ 16.1/shader_make.c
@@ -0,0 +1,36 @@
+#include <glib.h>
+#include <shader_compile.h>
+#include <shader_make.h>
+
+GLuint shader_make(void)
+{
+	GLuint vertex, fragment;
+	GLuint program;
+	GLint success;
+
+	// container
+	extern const void _binary____shader_container_vertex_glsl_start;
+	extern const void _binary____shader_container_vertex_glsl_end;
+	extern const void _binary____shader_container_fragment_glsl_start;
+	extern const void _binary____shader_container_fragment_glsl_end;
+
+	vertex = shader_compile(GL_VERTEX_SHADER, &_binary____shader_container_vertex_glsl_start, &_binary____shader_container_vertex_glsl_end - &_binary____shader_container_vertex_glsl_start);
+	fragment = shader_compile(GL_FRAGMENT_SHADER, &_binary____shader_container_fragment_glsl_start, &_binary____shader_container_fragment_glsl_end - &_binary____shader_container_fragment_glsl_start);
+
+	program = glCreateProgram();
+	glAttachShader(program, vertex);
+	glAttachShader(program, fragment);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE) {
+		GLchar message[512];
+
+		glGetProgramInfoLog(program, sizeof message, NULL, message);
+		g_error("Link error: %s\n", message);
+		program = 0;
+	}
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+
+	return program;
+}
