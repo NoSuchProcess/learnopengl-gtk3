e5f04fd4cd9937cff5b1e338a4d65a73be92a6e8
Sergey Stepanov <no.such.process@gmail.com>
2019-09-08 15:29:28 +0500

    8.20.1: fixup indentation

--- 8.20.1/main.c
+++ 8.20.1/main.c
@@ -5,7 +5,7 @@
 #include <glmath.h>
 
 static GLfloat vertices[] = {
-	// positions // colors // texture coords
+	// positions // colors       // texture coords
 	+0.5f, +0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,	// top right
 	+0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,	// bottom right
 	-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,	// bottom left
