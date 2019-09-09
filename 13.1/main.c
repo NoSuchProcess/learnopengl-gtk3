c111fbb3ddb645e4eb01519bb557a43b190620d0
Sergey Stepanov <no.such.process@gmail.com>
2019-09-10 01:28:56 +0500

    13.1: #5 fixup glDrawArrays() vertex count

--- 13.1/main.c
+++ 13.1/main.c
@@ -139,7 +139,7 @@ static gboolean render(GtkGLArea *area, GdkGLContext *context, gpointer user_dat
 	glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_FALSE, (const GLfloat *) &view);
 	glUniformMatrix4fv(glGetUniformLocation(program, "projection"), 1, GL_FALSE, (const GLfloat *) &projection);
 	glBindVertexArray(light_vao);
-	glDrawArrays(GL_TRIANGLES, 0, G_N_ELEMENTS(vertices));
+	glDrawArrays(GL_TRIANGLES, 0, G_N_ELEMENTS(vertices) / 3);
 
 	// Container
 	model = mat4_identity();
@@ -152,7 +152,7 @@ static gboolean render(GtkGLArea *area, GdkGLContext *context, gpointer user_dat
 	glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_FALSE, (const GLfloat *) &view);
 	glUniformMatrix4fv(glGetUniformLocation(program, "projection"), 1, GL_FALSE, (const GLfloat *) &projection);
 	glBindVertexArray(vao);
-	glDrawArrays(GL_TRIANGLES, 0, G_N_ELEMENTS(vertices));
+	glDrawArrays(GL_TRIANGLES, 0, G_N_ELEMENTS(vertices) / 3);
 
 	glBindVertexArray(0);
 	glUseProgram(0);
