b53242ca3ea8b214dd8cd5f8702593d443dffdd2
Sergey Stepanov <no.such.process@gmail.com>
2019-09-14 23:46:12 +0500

    lesson 16.3: fixup pvs warnings

--- 16.3/main.c
+++ 16.3/main.c
@@ -1,52 +1,59 @@
-#include <gtk/gtk.h>
+#include <stddef.h>
 #include <epoxy/gl.h>
-#include <shader_make.h>
+#include <gtk/gtk.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
+#include <shader_make.h>
 #include <glmath.h>
 
-static GLfloat vertices[] = {
-	// positions      // normals         // texture
-	+0.5f, +0.5f, +0.5f, +1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-	+0.5f, -0.5f, +0.5f, +1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-	+0.5f, +0.5f, -0.5f, +1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-	+0.5f, -0.5f, +0.5f, +1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-	+0.5f, -0.5f, -0.5f, +1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-	+0.5f, +0.5f, -0.5f, +1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+typedef struct {
+	vec3 position;
+	vec3 normal;
+	vec2 texture;
+} vertix;
 
-	+0.5f, +0.5f, +0.5f, 0.0f, +1.0f, 0.0f, 0.0f, 1.0f,
-	+0.5f, +0.5f, -0.5f, 0.0f, +1.0f, 0.0f, 1.0f, 1.0f,
-	-0.5f, +0.5f, -0.5f, 0.0f, +1.0f, 0.0f, 1.0f, 0.0f,
-	+0.5f, +0.5f, +0.5f, 0.0f, +1.0f, 0.0f, 0.0f, 1.0f,
-	-0.5f, +0.5f, -0.5f, 0.0f, +1.0f, 0.0f, 1.0f, 0.0f,
-	-0.5f, +0.5f, +0.5f, 0.0f, +1.0f, 0.0f, 0.0f, 0.0f,
+static vertix vertices[] = {
+	// positions         // normals           // texture
+	{{+0.5f, +0.5f, +0.5f}, {+1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+	{{+0.5f, -0.5f, +0.5f}, {+1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+	{{+0.5f, +0.5f, -0.5f}, {+1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+	{{+0.5f, -0.5f, +0.5f}, {+1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+	{{+0.5f, -0.5f, -0.5f}, {+1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+	{{+0.5f, +0.5f, -0.5f}, {+1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
 
-	-0.5f, -0.5f, +0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-	-0.5f, +0.5f, +0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-	-0.5f, +0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-	-0.5f, -0.5f, +0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-	-0.5f, +0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+	{{+0.5f, +0.5f, +0.5f}, {0.0f, +1.0f, 0.0f}, {0.0f, 1.0f}},
+	{{+0.5f, +0.5f, -0.5f}, {0.0f, +1.0f, 0.0f}, {1.0f, 1.0f}},
+	{{-0.5f, +0.5f, -0.5f}, {0.0f, +1.0f, 0.0f}, {1.0f, 0.0f}},
+	{{+0.5f, +0.5f, +0.5f}, {0.0f, +1.0f, 0.0f}, {0.0f, 1.0f}},
+	{{-0.5f, +0.5f, -0.5f}, {0.0f, +1.0f, 0.0f}, {1.0f, 0.0f}},
+	{{-0.5f, +0.5f, +0.5f}, {0.0f, +1.0f, 0.0f}, {0.0f, 0.0f}},
 
-	+0.5f, -0.5f, +0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-	-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
-	+0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-	+0.5f, -0.5f, +0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-	-0.5f, -0.5f, +0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+	{{-0.5f, -0.5f, +0.5f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+	{{-0.5f, +0.5f, +0.5f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+	{{-0.5f, +0.5f, -0.5f}, {-1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+	{{-0.5f, -0.5f, +0.5f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+	{{-0.5f, +0.5f, -0.5f}, {-1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+	{{-0.5f, -0.5f, -0.5f}, {-1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
 
-	+0.5f, -0.5f, +0.5f, 0.0f, 0.0f, +1.0f, 1.0f, 1.0f,
-	+0.5f, +0.5f, +0.5f, 0.0f, 0.0f, +1.0f, 1.0f, 0.0f,
-	-0.5f, +0.5f, +0.5f, 0.0f, 0.0f, +1.0f, 0.0f, 0.0f,
-	+0.5f, -0.5f, +0.5f, 0.0f, 0.0f, +1.0f, 1.0f, 1.0f,
-	-0.5f, +0.5f, +0.5f, 0.0f, 0.0f, +1.0f, 0.0f, 0.0f,
-	-0.5f, -0.5f, +0.5f, 0.0f, 0.0f, +1.0f, 0.0f, 1.0f,
+	{{+0.5f, -0.5f, +0.5f}, {0.0f, -1.0f, 0.0f}, {0.0f, 0.0f}},
+	{{-0.5f, -0.5f, -0.5f}, {0.0f, -1.0f, 0.0f}, {1.0f, 1.0f}},
+	{{+0.5f, -0.5f, -0.5f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f}},
+	{{+0.5f, -0.5f, +0.5f}, {0.0f, -1.0f, 0.0f}, {0.0f, 0.0f}},
+	{{-0.5f, -0.5f, +0.5f}, {0.0f, -1.0f, 0.0f}, {0.0f, 1.0f}},
+	{{-0.5f, -0.5f, -0.5f}, {0.0f, -1.0f, 0.0f}, {1.0f, 1.0f}},
 
-	+0.5f, +0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
-	+0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
-	+0.5f, +0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
-	-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
-	-0.5f, +0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
+	{{+0.5f, -0.5f, +0.5f}, {0.0f, 0.0f, +1.0f}, {1.0f, 1.0f}},
+	{{+0.5f, +0.5f, +0.5f}, {0.0f, 0.0f, +1.0f}, {1.0f, 0.0f}},
+	{{-0.5f, +0.5f, +0.5f}, {0.0f, 0.0f, +1.0f}, {0.0f, 0.0f}},
+	{{+0.5f, -0.5f, +0.5f}, {0.0f, 0.0f, +1.0f}, {1.0f, 1.0f}},
+	{{-0.5f, +0.5f, +0.5f}, {0.0f, 0.0f, +1.0f}, {0.0f, 0.0f}},
+	{{-0.5f, -0.5f, +0.5f}, {0.0f, 0.0f, +1.0f}, {0.0f, 1.0f}},
+
+	{{+0.5f, +0.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {0.0f, 0.0f}},
+	{{+0.5f, -0.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {0.0f, 1.0f}},
+	{{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {1.0f, 1.0f}},
+	{{+0.5f, +0.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {0.0f, 0.0f}},
+	{{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {1.0f, 1.0f}},
+	{{-0.5f, +0.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f}}
 };
 
 static const vec3 cubePositions[] = {
@@ -103,7 +110,7 @@ static void realize(GtkGLArea *area, gpointer user_data)
 	glBindVertexArray(light_vao);
 	glBindBuffer(GL_ARRAY_BUFFER, light_vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof (GLfloat), (void *) 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof (vertix), (const void *) offsetof(vertix, position));
 	glEnableVertexAttribArray(0);
 
 	program = shader_make(SHADER_SET_CONTAINER);
@@ -114,11 +121,11 @@ static void realize(GtkGLArea *area, gpointer user_data)
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof (GLfloat), (void *) 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof (vertix), (const void *) offsetof(vertix, position));
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof (GLfloat), (void *) (3 * sizeof (GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof (vertix), (const void *) offsetof(vertix, normal));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof (GLfloat), (void *) (6 * sizeof (GLfloat)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof (vertix), (const void *) offsetof(vertix, texture));
 	glEnableVertexAttribArray(2);
 
 	glGenTextures(2, texture);
@@ -193,7 +200,7 @@ static gboolean render(GtkGLArea *area, GdkGLContext *context, gpointer user_dat
 	glUniformMatrix4fv(glGetUniformLocation(light_program, "view"), 1, GL_FALSE, (const GLfloat *) &view);
 	glUniformMatrix4fv(glGetUniformLocation(light_program, "projection"), 1, GL_FALSE, (const GLfloat *) &projection);
 	glBindVertexArray(light_vao);
-	glDrawArrays(GL_TRIANGLES, 0, G_N_ELEMENTS(vertices) / 8);
+	glDrawArrays(GL_TRIANGLES, 0, G_N_ELEMENTS(vertices));
 
 	// Container
 	glUseProgram(program);
@@ -225,7 +232,7 @@ static gboolean render(GtkGLArea *area, GdkGLContext *context, gpointer user_dat
 	for (unsigned int i = 0; i < G_N_ELEMENTS(cubePositions); i++) {
 		const mat4 model = mat4_mul(mat4_translation(cubePositions[i]), mat4_rotation(to_radians(20.0f * i), (vec3){1.0f, 0.3f, 0.5f}));
 		glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, (const GLfloat *) &model);
-		glDrawArrays(GL_TRIANGLES, 0, G_N_ELEMENTS(vertices) / 8);
+		glDrawArrays(GL_TRIANGLES, 0, G_N_ELEMENTS(vertices));
 	}
 
 	glBindVertexArray(0);
