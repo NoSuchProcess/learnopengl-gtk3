87cde21be6368334cbaec6176c12f3f5453ee8e8
Sergey Stepanov <no.such.process@gmail.com>
2019-09-14 21:22:10 +0500

    lesson 16.6: fixup compile warnings

--- 16.6/main.c
+++ 16.6/main.c
@@ -67,9 +67,9 @@ static GLuint vbo;
 static GLuint program;
 static GLuint texture[2];
 
-static vec3 cameraPos = (vec3) {0.0f, 0.0f, 5.0f};
-static vec3 cameraFront = (vec3) {0.0f, 0.0f, -1.0f};
-static vec3 cameraUp = (vec3) {0.0f, 1.0f, 0.0f};
+static vec3 cameraPos = {0.0f, 0.0f, 5.0f};
+static vec3 cameraFront = {0.0f, 0.0f, -1.0f};
+static vec3 cameraUp = {0.0f, 1.0f, 0.0f};
 
 #define FOV_MAX 60.
 #define FOV_MIN 1.
@@ -100,7 +100,7 @@ static void realize(GtkGLArea *area, gpointer user_data)
 	glEnableVertexAttribArray(2);
 
 	glGenTextures(2, texture);
-	for (int i = 0; i < G_N_ELEMENTS(texture); ++i) {
+	for (unsigned int i = 0; i < G_N_ELEMENTS(texture); ++i) {
 		static const char *const filename[G_N_ELEMENTS(texture)] = {
 			"container.png",
 			"container_specular.png",
@@ -187,7 +187,7 @@ static gboolean render(GtkGLArea *area, GdkGLContext *context, gpointer user_dat
 	glBindTexture(GL_TEXTURE_2D, texture[0]);
 	glActiveTexture(GL_TEXTURE1);
 	glBindTexture(GL_TEXTURE_2D, texture[1]);
-	for (int i = 0; i < G_N_ELEMENTS(cubePositions); i++) {
+	for (unsigned int i = 0; i < G_N_ELEMENTS(cubePositions); i++) {
 		const mat4 model = mat4_mul(mat4_translation(cubePositions[i]), mat4_rotation(to_radians(20.0f * i), (vec3){1.0f, 0.3f, 0.5f}));
 		glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, (const GLfloat *) &model);
 		glDrawArrays(GL_TRIANGLES, 0, G_N_ELEMENTS(vertices) / 8);
