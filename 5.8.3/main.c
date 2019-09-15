d377b00457750965f4950191f5d02949b1fe0b50
Sergey Stepanov <no.such.process@gmail.com>
2019-09-15 18:50:48 +0500

    lesson 5.8.3: fixup argument type cast

--- 5.8.3/main.c
+++ 5.8.3/main.c
@@ -42,7 +42,7 @@ static void realize(GtkGLArea *area, gpointer user_data)
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof vertices1, vertices1, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const void *) offsetof(vertex, position));
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, position));
 	glEnableVertexAttribArray(0);
 
 	glBindVertexArray(vao[1]);
@@ -50,7 +50,7 @@ static void realize(GtkGLArea *area, gpointer user_data)
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof vertices2, vertices2, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const void *) offsetof(vertex, position));
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, position));
 	glEnableVertexAttribArray(0);
 
 	glBindVertexArray(0);
