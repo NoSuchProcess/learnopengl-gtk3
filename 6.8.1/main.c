9aa9690ceb3e7b0717eee7eb7e3b5f9e38300e75
Sergey Stepanov <no.such.process@gmail.com>
2020-05-02 16:53:18 +0500

    6.8.1: remove 'layout (location = ..)' in shader

--- 6.8.1/main.c
+++ 6.8.1/main.c
@@ -30,20 +30,26 @@ static void realize(GtkGLArea *area, gpointer user_data)
 
 	program = shader_make();
 
-	glGenVertexArrays(1, &vao);
-	glGenBuffers(1, &vbo);
+	{
+		GLint index;
 
-	glBindVertexArray(vao);
+		glGenVertexArrays(1, &vao);
+		glGenBuffers(1, &vbo);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
+		glBindVertexArray(vao);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, position));
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, color));
-	glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
 
-	glBindVertexArray(0);
+		index = glGetAttribLocation(program, "position");
+		glVertexAttribPointer(index, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, position));
+		glEnableVertexAttribArray(index);
+		index = glGetAttribLocation(program, "color");
+		glVertexAttribPointer(index, 3, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, color));
+		glEnableVertexAttribArray(index);
+
+		glBindVertexArray(0);
+	}
 }
 
 static void unrealize(GtkGLArea *area, gpointer user_data)
