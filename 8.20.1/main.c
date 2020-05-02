9da93a275e8bbf0889c5fe30e2c5b8d1f7b8bbde
Sergey Stepanov <no.such.process@gmail.com>
2020-05-02 16:53:18 +0500

    8.20.1: remove 'layout (location = ..)' in shader

--- 8.20.1/main.c
+++ 8.20.1/main.c
@@ -79,26 +79,34 @@ static void realize(GtkGLArea *area, gpointer user_data)
 		g_object_unref(G_OBJECT(pixbuf));
 	}
 
-	glBindVertexArray(vao);
+	{
+		GLint index;
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
+		glBindVertexArray(vao);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, position));
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, color));
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, texture));
-	glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices, GL_STATIC_DRAW);
+
+		index = glGetAttribLocation(program, "position");
+		glVertexAttribPointer(index, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, position));
+		glEnableVertexAttribArray(index);
+		index = glGetAttribLocation(program, "color");
+		glVertexAttribPointer(index, 3, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, color));
+		glEnableVertexAttribArray(index);
+		index = glGetAttribLocation(program, "tex_coord");
+		glVertexAttribPointer(index, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (const GLvoid *) offsetof(vertex, texture));
+		glEnableVertexAttribArray(index);
+
+		glBindVertexArray(0);
+	}
 
 	glUseProgram(program);
 	glUniform1i(glGetUniformLocation(program, "texture1"), 0);
 	glUniform1i(glGetUniformLocation(program, "texture2"), 1);
-
-	glBindVertexArray(0);
+	glUseProgram(program);
 }
 
 static void unrealize(GtkGLArea *area, gpointer user_data)
